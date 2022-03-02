07eecf3e4382da76e04d903b6a82aa47386b9a0d
Mithin <43065512+Mint25@users.noreply.github.com>
2022-03-02 17:52:14 +0530

    script

---
+++ script.sh
@@ -0,0 +1,18 @@
+          echo what is in the direcotry?
+          ls -a 
+          echo is Java installed?
+          java -version
+          echo is git installed?
+          git --version
+          echo build tools?
+          mvn --version
+          gradle --version 
+          echo what is the workspace location 
+          echo $RUNNER_WORKSPACE
+          echo who is running this script 
+          whoami
+          echo how is the disc laid out ?
+          df
+          echo evnvironment variables ?
+          env
+          
